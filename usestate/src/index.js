d1d5c7ddbc2a4970a67437af2a9ee3662254373f
Yogesh Krishna Chavan <yogeshchavan113@gmail.com>
2024-11-04 22:21:44 +0530

    Update in react hooks

--- usestate/src/index.js
+++ usestate/src/index.js
@@ -2,16 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 // import App from "./App";
-// import Data from "./Data";
-import Form from "./Form";
-import FormData from "./FormData";
+import Data from "./Data";
+// import Form from "./Form";
+// import FormData from "./FormData";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    {/* <Data /> */}
+    <Data />
     {/* <Form /> */}
-    <FormData />
+    {/* <FormData /> */}
   </React.StrictMode>
 );
