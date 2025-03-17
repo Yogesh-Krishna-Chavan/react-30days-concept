8627344ed4cf4ee33a402d2a0e28137dc75a2923
Yogesh Krishna Chavan <yogeshchavan113@gmail.com>
2025-03-17 23:05:42 +0530

    Hello React with Jsx

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+function App() {
+  return (
+    <div className="hero">
+      <h1 style={{ color: "red" }}>Hello React</h1>
+      <h2>Hello React</h2>
+    </div>
+  );
+}
+
+export default App;
