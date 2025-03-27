9b23e71b86032f5f047a5ffd07b5337bf9310b4b
Yogesh Krishna Chavan <yogeshchavan113@gmail.com>
2025-03-27 11:02:09 +0530

    Fetch in react

--- use-effect/src/App.js
+++ use-effect/src/App.js
@@ -1,12 +1,17 @@
 import React, { useEffect, useState } from "react";
 
+//useState and useEffect code
+
 const App = () => {
   const [clicked, setClicked] = useState("");
   const [count, setCount] = useState(0);
 
   useEffect(() => {
-    alert("We are using useState");
-  }, [count]);
+    console.log("We are using useState");
+    return () => {
+      console.log("Return of use state");
+    };
+  }, [count, clicked]);
 
   return (
     <div>
